a44f9b3abdd858d1985797e7452475b01f54d109
Gargi Gundawar <gargipgundawar@gmail.com>
2026-04-22 16:13:01 +0530

    SHOP-7: added basic validation

--- backend/app.py
+++ backend/app.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
 
 # backend/app.py
 def process_payment(amount):
-    if amount > 0:
-        return "Success"
+    if amount <= 0:
+        return "Invalid amount"
+    return "Success"
